eafab0e70038de6482868de5e332aaebbf204ba0
mangonob <mangonob@qq.com>
2021-12-15 23:29:15 +0800

    feat: float functions

--- floatProcess.c
+++ floatProcess.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
 extern double mpow(double a, int n);
+extern double msin(double rad);
+extern double mcos(double rad);
+extern double msqrt(double rad);
+extern double mpi;
 
 int main(int argc, char *argv[])
 {
-    printf("%lf\n", mpow(2, 10));
+    double a;
+
+    while (1)
+    {
+        printf("angle = ? ");
+        scanf("%lf", &a);
+        printf("sin(%g) = %g\n", a, msin(a));
+        printf("cos(%g) = %g\n", a, mcos(a));
+    }
+
     return 0;
 }
--- floatProcess_.asm
+++ floatProcess_.asm
@@ -1,10 +1,17 @@
 ; ----------------------------------------------------------------
 ;
-;  double _pow(double a, int n);
+; double _mpow(double a, int n);
+; double _msin(double rad);
+; double _mcos(double rad);
+; double _msqrt(double a);
 ;
 ; ----------------------------------------------------------------
 
     global  _mpow
+    global  _msin
+    global  _mcos
+    global  _mpi
+    global  _msqrt
 
 _mpow:
     push    rbp
@@ -35,3 +42,46 @@ _mpow:
     pop     rdi
     pop     rbp
     ret
+
+_msin:
+    push    rbp
+    mov     rbp, rsp
+
+    movsd   qword [rbp-8], xmm0
+    fld     qword [rbp-8]
+    fsin
+    fstp    qword [rbp-8]
+    movsd   xmm0, qword [rbp-8]
+
+    pop     rbp
+    ret
+
+_mcos:
+    push    rbp
+    mov     rbp, rsp
+
+    movsd   qword [rbp-8], xmm0
+    fld     qword [rbp-8]
+    fcos
+    fstp    qword [rbp-8]
+    movsd   xmm0, qword [rbp-8]
+
+    pop     rbp
+    ret
+
+_msqrt:
+    push    rbp
+    mov     rbp, rsp
+
+    movsd   qword [rbp-8], xmm0
+    fld     qword [rbp-8]
+    fsqrt
+    fstp    qword [rbp-8]
+    movsd   xmm0, qword [rbp-8]
+
+    pop     rbp
+    ret
+
+    section .data
+_mpi:
+    dq      3.141592653589793
